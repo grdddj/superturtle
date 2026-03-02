70cbc2ceca79d7a5c3251149ed7c42ae9f706fe1
Richard Mladek <rigospigos@gmail.com>
2026-03-02 20:21:26 +0100

    chore: extract linkedin-demo to standalone repository

--- linkedin-demo/src/hooks/useConvexPosts.js
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "convex/react";
-import { api } from "../convex/_generated/api";
-
-const useConvexPosts = (sortBy = "recent", options = {}) => {
-  const posts = useQuery(api.posts.listPosts, { sortBy, ...options });
-  return posts;
-};
-
-export default useConvexPosts;
